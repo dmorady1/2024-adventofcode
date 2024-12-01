7340fdde47790e3f3b7490eb5e8f5c3bab7b694d
David Morady <29502285+dmorady1@users.noreply.github.com>
2024-12-01 20:29:57 +0100

    refactor day1

--- day1/main.go
+++ day1/main.go
@@ -11,10 +11,45 @@ import (
 )
 
 func main() {
-	file, err := os.Open("input.txt")
+	left, right, err := readInput(os.Args[1])
 	if err != nil {
 		fmt.Println("Error:", err)
-		return
+	}
+
+	slices.Sort(left)
+	slices.Sort(right)
+	result := calculatePart1(left, right)
+	fmt.Printf("part1: %d\n", result)
+
+	result2 := calculatePart2(left, right)
+	fmt.Printf("part2: %d\n", result2)
+}
+func calculatePart1(left, right []int) int {
+	result := 0
+	for i := 0; i < len(left); i++ {
+		result += int(math.Abs(float64(left[i] - right[i])))
+	}
+	return result
+}
+
+func calculatePart2(left, right []int) int {
+	m := make(map[int]int)
+
+	for _, value := range right {
+		m[value] += 1
+	}
+	result := 0
+	for _, value := range left {
+		result += value * m[value]
+	}
+	return result
+}
+
+func readInput(filename string) ([]int, []int, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return nil, nil, err
 	}
 	defer file.Close()
 
@@ -25,31 +60,16 @@ func main() {
 	for scanner.Scan() {
 		line := scanner.Text()
 		numbersPerLine := s.Fields(line)
-		leftNumber, _ := strconv.Atoi(numbersPerLine[0])
-		rightNumber, _ := strconv.Atoi(numbersPerLine[1])
+		leftNumber, err := strconv.Atoi(numbersPerLine[0])
+		if err != nil {
+			return nil, nil, fmt.Errorf("invalid numbers: %s", numbersPerLine[0])
+		}
+		rightNumber, err := strconv.Atoi(numbersPerLine[1])
+		if err != nil {
+			return nil, nil, fmt.Errorf("invalid numbers: %s", numbersPerLine[1])
+		}
 		left = append(left, leftNumber)
 		right = append(right, rightNumber)
 	}
-
-	slices.Sort(left)
-	slices.Sort(right)
-	result := 0
-	for i := 0; i < len(left); i++ {
-		result += int(math.Abs(float64(left[i] - right[i])))
-
-	}
-	fmt.Printf("part1: %d\n", result)
-
-	m := make(map[int]int)
-
-	for _, value := range right {
-		m[value] += 1
-	}
-
-	result2 := 0
-	for _, value := range left {
-		result2 += value * m[value]
-	}
-
-	fmt.Printf("part2: %d\n", result2)
+	return left, right, nil
 }
